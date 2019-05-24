bfb133cf90ad80fc41735616c9b946e63d5b7f25
Dimash Kamerdinov <dimash.k@internal.Tabella.cz>
2019-05-24 16:05:39 +0200

    specified webpack for tabella

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,20 +1,20 @@
-const path = require('path');
-const postcssPresetEnv = require('postcss-preset-env');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require("path");
+const postcssPresetEnv = require("postcss-preset-env");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry: {
-    app: './src/index.js'
+    app: "./src/index.js"
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.[contenthash].js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.[contenthash].js"
   },
   // devtool: 'source-map',
   module: {
@@ -24,9 +24,9 @@ module.exports = {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
+            presets: ["@babel/preset-env"]
           }
         }
       },
@@ -36,19 +36,19 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               sourceMap: true
             }
           },
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
-              ident: 'postcss',
+              ident: "postcss",
               plugins: () => [
                 postcssPresetEnv({
                   autoprefixer: {
-                    browsers: ['last 4 version']
+                    browsers: ["last 4 version"]
                   }
                 })
               ],
@@ -56,7 +56,7 @@ module.exports = {
             }
           },
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
               sourceMap: true
             }
@@ -68,14 +68,14 @@ module.exports = {
         test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[path][name].[ext]?[hash]',
-              outputPath: 'images/'
+              name: "[path][name].[ext]?[hash]",
+              outputPath: "images/"
             }
           },
           {
-            loader: 'image-webpack-loader',
+            loader: "image-webpack-loader",
             options: {
               mozjpeg: {
                 progressive: true,
@@ -86,7 +86,7 @@ module.exports = {
                 enabled: false
               },
               pngquant: {
-                quality: '75-90',
+                quality: "75-90",
                 speed: 4
               },
               gifsicle: {
@@ -101,10 +101,10 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[path][name].[ext]?[hash]',
-              outputPath: 'fonts/'
+              name: "[path][name].[ext]?[hash]",
+              outputPath: "fonts/"
             }
           }
         ]
@@ -114,23 +114,21 @@ module.exports = {
         test: /\.(html|php)$/,
         use: [
           {
-            loader: 'html-loader',
-            options: {
-              minimize: true
-            }
+            loader: "html-loader",
+            options: {}
           }
         ]
       }
     ]
   },
   plugins: [
-    new CleanWebpackPlugin(['dist/*']),
+    new CleanWebpackPlugin(["dist/*"]),
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/index.html'
+      filename: "index.php",
+      template: "src/index.html"
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css'
+      filename: "[name].[contenthash].css"
     }),
     new webpack.ProvidePlugin({
       // $: 'jquery',
@@ -140,38 +138,38 @@ module.exports = {
   optimization: {
     minimizer: [new UglifyJsPlugin({}), new OptimizeCSSAssetsPlugin({})],
     splitChunks: {
-      chunks: 'all'
+      chunks: "all"
     }
   },
   resolve: {
     alias: {
       TweenLite: path.resolve(
-        'node_modules',
-        'gsap/src/minified/TweenLite.min.js'
+        "node_modules",
+        "gsap/src/minified/TweenLite.min.js"
       ),
       TweenMax: path.resolve(
-        'node_modules',
-        'gsap/src/minified/TweenMax.min.js'
+        "node_modules",
+        "gsap/src/minified/TweenMax.min.js"
       ),
       TimelineLite: path.resolve(
-        'node_modules',
-        'gsap/src/minified/TimelineLite.min.js'
+        "node_modules",
+        "gsap/src/minified/TimelineLite.min.js"
       ),
       TimelineMax: path.resolve(
-        'node_modules',
-        'gsap/src/minified/TimelineMax.min.js'
+        "node_modules",
+        "gsap/src/minified/TimelineMax.min.js"
       ),
       ScrollMagic: path.resolve(
-        'node_modules',
-        'scrollmagic/scrollmagic/minified/ScrollMagic.min.js'
+        "node_modules",
+        "scrollmagic/scrollmagic/minified/ScrollMagic.min.js"
       ),
-      'animation.gsap': path.resolve(
-        'node_modules',
-        'scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js'
+      "animation.gsap": path.resolve(
+        "node_modules",
+        "scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js"
       ),
-      'debug.addIndicators': path.resolve(
-        'node_modules',
-        'scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js'
+      "debug.addIndicators": path.resolve(
+        "node_modules",
+        "scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js"
       )
     }
   }
